3eaf6cb096106f5684a34c46ec3461206c79c6f8
Jagrath <jagrathshettylxix@gmail.com>
2022-06-28 10:00:04 +0530

    hero section updated

--- Components/HeroSection.jsx
+++ Components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const HeroSection = () => {
@@ -5,7 +6,7 @@ const HeroSection = () => {
     <div className=" h-[95vh] w-full flex flex-col justify-around items-center px-6  ">
       {/* Hero Text */}
       <div className="flex flex-col justify-around items-baseline h-[45vh] pt-5  w-full">
-        <div>
+        <div className="-space-y-1">
           <h2 className="text-twoColor font-bold text-3xl">Hi there! I'm</h2>
           <h1 className="text-threeColor font-bold text-4xl ">
             Jagrath Shetty
@@ -21,13 +22,15 @@ const HeroSection = () => {
         </h2>
 
         {/* Github Button CTA */}
-        <button className="bg-fourColor/30 border border-threeColor py-2 px-6 text-threeColor text-2xl font-semibold ">
+        <button className="bg-threeColor/20  border-2 border-threeColor py-2 px-6 mt-2 text-threeColor text-2xl font-semibold active:scale-95 transition-all duration-150">
           View Github
         </button>
       </div>
 
       {/* Hero Image */}
-      <div className="bg-red-100 h-[40vh] w-full my-7">Image</div>
+      <div className=" h-[40vh] w-full  bg-twoColor flex justify-center items-center">
+        <Image src="/hero-image.svg" width="400" height="400" />
+      </div>
     </div>
   );
 };
