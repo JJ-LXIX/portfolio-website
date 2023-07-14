6ab8ae2dd67f25feeb44b6fea48d97f284f24be7
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-14 18:25:44 +0530

    added dental website to work section

--- Components/WorkSection.jsx
+++ Components/WorkSection.jsx
@@ -12,6 +12,8 @@ import NFTCollection from "../public/portfolio-work/NFTcollection.png";
 import ShoeImage from "../public/portfolio-work/shoe.png";
 import Jwitter from "../public/portfolio-work/jwitter.jpg";
 import Jwitter2 from "../public/portfolio-work/jwitter2.jpg";
+import DentalWebsite1 from "../public/portfolio-work/dental1.png";
+import DentalWebsite2 from "../public/portfolio-work/dental2.png";
 
 const WorkSection = () => {
   return (
@@ -83,6 +85,7 @@ const WorkSection = () => {
           </div>
         </div>
       </div>
+
       {/* 2nd Project */}
       <div className=" w-full mt-10 flex flex-col items-center">
         <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10">
@@ -151,8 +154,71 @@ const WorkSection = () => {
 
       {/* 3rd Project */}
       <div className="mx-2 mt-10 flex flex-col items-center">
+        <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10 ">
+          <span className="text-5xl md:text-7xl font-light">03.</span> Dentist
+          Website
+        </h2>
+
+        {/* inner text */}
+        <div className="w-[90%] border-l-2 border-r-2 border-l-threeColor flex flex-col items-center my-6 mb-10 mx-4 ">
+          <h3 className="text-threeColor font-semibold text-lg mt-8 md:text-2xl text-center">
+            A simple website for a Dentist{" "}
+          </h3>
+          <div className="flex flex-col lg:flex-row lg:justify-around lg:w-full">
+            {/* Website Image */}
+            <div className="border-2 border-twoColor h-44 w-72 my-6 relative md:h-80 md:w-[35rem] lg:h-[30rem] lg:w-[50rem]">
+              <Image
+                src={DentalWebsite1}
+                layout="fill"
+                objectFit="contain"
+                placeholder="blur"
+                alt="Screenshot of a Dental Website that I made"
+              />
+            </div>
+
+            <div className="border-2 border-twoColor h-44 w-72 my-6 relative md:h-80 md:w-[35rem] lg:h-[30rem] lg:w-[50rem]">
+              <Image
+                src={DentalWebsite2}
+                layout="fill"
+                objectFit="contain"
+                placeholder="blur"
+                alt="Screenshot of a Dental Website that I made"
+              />
+            </div>
+          </div>
+
+          <h3 className="text-sm text-threeColor font-semibold md:text-2xl text-center ">
+            Tech Stack : NextJS, Framer Motion, Tailwind, ShadcnUI
+          </h3>
+
+          {/* CTA Buttons*/}
+          <div className="flex mt-4 flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4 items-center lg:space-x-10">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/JJ-LXIX/dental-client-site"
+            >
+              <button className="bg-transparent  border-2 border-threeColor  w-60  py-3 px-6 lg:py-4 lg:w-64  text-threeColor text-2xl font-semibold  hover:cursor-none lg:hover:scale-110 active:scale-95 lg:active:scale-90 transition-all duration-100 lg:duration-200">
+                View on Github
+              </button>
+            </a>
+            <a
+              href="https://dental-client-site.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="bg-twoColor/50  border-2 border-threeColor  w-60  py-3 px-6 lg:py-4 lg:w-64 md:px-14 text-threeColor text-2xl font-semibold hover:cursor-none lg:hover:scale-110  active:scale-90 lg:active:scale-90 transition-all duration-100 lg:duration-200">
+                View Site
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* 4th Project */}
+      <div className="mx-2 mt-10 flex flex-col items-center">
         <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10">
-          <span className="text-5xl md:text-7xl font-light">03.</span> Wordle
+          <span className="text-5xl md:text-7xl font-light">04.</span> Wordle
           Clone
         </h2>
 
@@ -216,10 +282,10 @@ const WorkSection = () => {
         </div>
       </div>
 
-      {/* 4th Project */}
+      {/* 5th Project */}
       <div className="mx-2 mt-10 flex flex-col items-center">
         <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10 ">
-          <span className="text-5xl md:text-7xl font-light">04.</span> Crypto
+          <span className="text-5xl md:text-7xl font-light">05.</span> Crypto
           Tracker
         </h2>
 
@@ -279,10 +345,10 @@ const WorkSection = () => {
         </div>
       </div>
 
-      {/* 5th Project */}
+      {/* 6th Project */}
       <div className="mx-2 mt-10 flex flex-col items-center">
         <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10 ">
-          <span className="text-5xl md:text-7xl font-light">05.</span> NFT
+          <span className="text-5xl md:text-7xl font-light">06.</span> NFT
           Project (NOTMYART)
         </h2>
 
@@ -397,7 +463,7 @@ const WorkSection = () => {
       {/* 7th Project */}
       <div className="mx-2 mt-10 flex flex-col items-center">
         <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10">
-          <span className="text-5xl md:text-7xl font-light">06.</span>{" "}
+          <span className="text-5xl md:text-7xl font-light">07.</span>{" "}
           Interactive Shoe Color Selector
         </h2>
 
