49cb3f700dc98ececd206cbf5d640dc9ed1a1cef
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-25 16:34:25 +0530

    added 3rd pokemon project to workSection

--- Components/WorkSection.jsx
+++ Components/WorkSection.jsx
@@ -14,6 +14,8 @@ import Jwitter from "../public/portfolio-work/jwitter.jpg";
 import Jwitter2 from "../public/portfolio-work/jwitter2.jpg";
 import DentalWebsite1 from "../public/portfolio-work/dental1.png";
 import DentalWebsite2 from "../public/portfolio-work/dental2.png";
+import poke1 from "../public/portfolio-work/poke1.jpg";
+import poke2 from "../public/portfolio-work/poke2.jpg";
 
 const WorkSection = () => {
   return (
@@ -151,8 +153,71 @@ const WorkSection = () => {
           </div>
         </div>
       </div>
-
       {/* 3rd Project */}
+      <div className=" w-full mt-10 flex flex-col items-center">
+        <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10">
+          <span className="text-5xl md:text-7xl font-light">03.</span>{" "}
+          Interactive Pokemon Website
+        </h2>
+
+        {/* inner text */}
+        <div className="w-[90%] border-l-2 border-r-2 border-threeColor flex flex-col items-center my-6 mb-10 mx-4 ">
+          <h3 className="text-threeColor font-semibold text-lg text-center mt-8 md:text-xl">
+            Interactive generation 1 starter pokemon cards{" "}
+          </h3>
+
+          {/* Website Image */}
+
+          <div className="flex flex-col lg:flex-row lg:justify-around lg:w-full ">
+            <div className="border-2 border-twoColor h-48 w-72 my-6 relative md:h-80 md:w-[35rem] lg:h-[30rem] lg:w-[50rem]">
+              <Image
+                src={poke1}
+                layout="fill"
+                objectFit="contain"
+                placeholder="blur"
+                alt="Screenshot of Interactive Pokemon site demo that I made"
+              />
+            </div>
+
+            <div className=" border-2 border-twoColor h-48 w-72 my-6 relative md:h-80 md:w-[35rem] lg:h-[30rem] lg:w-[50rem]">
+              <Image
+                src={poke2}
+                layout="fill"
+                objectFit="contain"
+                placeholder="blur"
+                alt="Screenshot of Interactive Pokemon site demo that I made"
+              />
+            </div>
+          </div>
+
+          <h3 className="text-sm text-threeColor font-semibold md:text-2xl text-center">
+            Tech Stack : Vite, Tailwind, React Three Fiber, React Three Drei
+          </h3>
+
+          {/* CTA Buttons*/}
+          <div className="flex mt-4 flex-col  space-y-4 md:space-y-0 md:flex-row md:space-x-4 items-center lg:space-x-10">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/JJ-LXIX/poke-r3f-vite"
+            >
+              <button className="bg-transparent  border-2 border-threeColor  w-60  py-3 px-6 lg:py-4 lg:w-64  text-threeColor text-2xl font-semibold  hover:cursor-none lg:hover:scale-110 active:scale-95 lg:active:scale-90 transition-all duration-100 lg:duration-200">
+                View on Github
+              </button>
+            </a>
+            <a
+              href="https://poke-r3f.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="bg-twoColor/50  border-2 border-threeColor  w-60  py-3 px-6 lg:py-4 lg:w-64 md:px-14 text-threeColor text-2xl font-semibold hover:cursor-none lg:hover:scale-110  active:scale-90 lg:active:scale-90 transition-all duration-100 lg:duration-200">
+                View Site
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
       {/* 4th Project */}
       <div className="mx-2 mt-10 flex flex-col items-center">
         <h2 className="text-threeColor font-bold text-3xl md:text-4xl self-start pl-10 ">
